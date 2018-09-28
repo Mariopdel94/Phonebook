be0e0a70471b2b830f67a26c2ee470949ec46faf
Mario Pineda <mariopdel94@gmail.co>
2018-09-28 00:18:47 -0600

    Prepared basic observables to interact between components.

--- phonebook-app/src/app/phones/create-phone/create-phone.component.ts
+++ phonebook-app/src/app/phones/create-phone/create-phone.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Contact, ContactService } from '../../_model/contact';
 
 @Component({
   selector: 'app-create-phone',
   templateUrl: './create-phone.component.html',
   styleUrls: ['./create-phone.component.scss']
 })
-export class CreatePhoneComponent implements OnInit {
+export class CreatePhoneComponent implements OnInit, OnDestroy {
+  @Input() public selectedRecord$: Subject<Contact>;
+  @Output() isEditing: EventEmitter<boolean> = new EventEmitter();
+  @Output() contactSaved: EventEmitter<boolean> = new EventEmitter();
+  // @ViewChild('saveForm') public recordForm: any;
+  public contact = new Contact();
+  public destroyed$ = new Subject();
 
-  constructor() { }
+  constructor(
+    private contactService: ContactService,
+  ) { }
 
   ngOnInit() {
+    this.contactSelectionListener();
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
+
+  private contactSelectionListener() {
+    this.selectedRecord$
+    .takeUntil(this.destroyed$)
+    .subscribe(contact => {
+      if (contact.id && contact.id > 0) {
+        this.retrieveContact(contact.id);
+      } else {
+        this.contact = new Contact();
+        // this.recordForm.reset();
+      }
+    }, error => {
+      console.log('Error: ', error);
+    });
+  }
+
+  private retrieveContact(id: number) {
+    // this.contactService.getContact(id)
+    // .takeUntil(this.destroyed$)
+    // .subscribe(response => {
+    //   this.contact = response.contact;
+    // }, error => {
+    //   console.log('Error: ', error);
+    // });
   }
 
 }
--- phonebook-app/src/app/phones/phone-overview/phone-overview.component.ts
+++ phonebook-app/src/app/phones/phone-overview/phone-overview.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { CreatePhoneComponent } from '../create-phone/create-phone.component';
+import { PhoneCatalogComponent } from '../phone-catalog/phone-catalog.component';
+import { Subject } from 'rxjs/Subject';
+import { Contact } from '../../_model/contact/contact';
 
 @Component({
   selector: 'app-phone-overview',
   templateUrl: './phone-overview.component.html',
   styleUrls: ['./phone-overview.component.scss']
 })
-export class PhoneOverviewComponent implements OnInit {
+export class PhoneOverviewComponent implements OnInit, OnDestroy {
+  @ViewChild(CreatePhoneComponent) public createPhoneComponent: CreatePhoneComponent;
+  @ViewChild(PhoneCatalogComponent) public phoneCatalogComponent: PhoneCatalogComponent;
+  public destroyed$ = new Subject();
+  public isEditing: boolean;
+  public selectedRecord: Contact = null;
+  public selectedRecord$: Subject<Contact> =  new Subject();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
+
+  closeEditSection(): void {
+    this.isEditing = false;
+  }
+
+  createHandler(): void {
+    this.isEditing = true;
+    this.selectedRecord = new Contact();
+    this.selectedRecord$.next(this.selectedRecord);
+  }
+
+  editHandler(record: Contact): void {
+    this.isEditing = true;
+    this.selectedRecord = record.clone();
+    this.selectedRecord$.next(this.selectedRecord);
+  }
+
+  onisEditing(isActive: boolean): void {
+    this.isEditing = isActive;
+  }
+
+  onSave() {
+    // this.phoneCatalogComponent.getContactList();
   }
 
 }
